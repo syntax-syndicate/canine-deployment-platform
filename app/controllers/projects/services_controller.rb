0f8f8a2bf2899ff700a99ca91935ad2de016c5df
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-09-30 11:26:00 -0700

    fix services bugs

--- app/controllers/projects/services_controller.rb
+++ app/controllers/projects/services_controller.rb
@@ -3,45 +3,45 @@ class Projects::ServicesController < Projects::BaseController
   before_action :set_service, only: %i[update destroy]
 
   def index
-    @services = @project.services
+    @services = @project.project_services
   end
 
   def new
-    @service = @project.services.build
+    @service = @project.project_services.build
   end
 
   def create
-    service = @project.services.build(service_params)
+    service = @project.project_services.build(service_params)
     if service.save
-      redirect_to project_services_path(@project), notice: 'Service was successfully created.'
+      redirect_to project_services_path(@project), notice: "Service was successfully created."
     else
-      redirect_to project_services_path(@project), alert: 'Service could not be created.'
+      redirect_to project_services_path(@project), alert: "Service could not be created."
     end
   end
 
   def update
     if @service.update(service_params)
-      redirect_to project_services_path(@project), notice: 'Service was successfully updated.'
+      redirect_to project_services_path(@project), notice: "Service was successfully updated."
     else
-      redirect_to project_services_path(@project), alert: 'Service could not be updated.'
+      redirect_to project_services_path(@project), alert: "Service could not be updated."
     end
   end
 
   def destroy
     if @service.destroy
-      redirect_to project_services_path(@project), notice: 'Service was successfully destroyed.'
+      redirect_to project_services_path(@project), notice: "Service was successfully destroyed."
     else
-      redirect_to project_services_path(@project), alert: 'Service could not be destroyed.'
+      redirect_to project_services_path(@project), alert: "Service could not be destroyed."
     end
   end
 
   private
 
   def set_service
-    @service = @project.services.find(params[:id])
+    @service = @project.project_services.find(params[:id])
   end
 
   def service_params
-    params.require(:service).permit(:service_type, :command, :name)
+    params.require(:project_service).permit(:service_type, :command, :name)
   end
 end
