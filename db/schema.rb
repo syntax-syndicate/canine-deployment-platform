d56026b7510b321cf931d122235a15d8221a3bdc
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-10-03 12:52:40 -0700

    fix connected accounts

--- db/schema.rb
+++ db/schema.rb
@@ -85,21 +85,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_03_163826) do
     t.index ["user_id"], name: "index_clusters_on_user_id"
   end
 
-  create_table "connected_providers", force: :cascade do |t|
-    t.string "owner_type"
-    t.bigint "owner_id"
-    t.string "access_token"
-    t.string "access_token_secret"
-    t.text "auth"
-    t.string "provider"
-    t.string "refresh_token"
-    t.string "uid"
-    t.datetime "expires_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["owner_type", "owner_id"], name: "index_connected_providers_on_owner"
-  end
-
   create_table "cron_schedules", force: :cascade do |t|
     t.bigint "service_id", null: false
     t.string "schedule", null: false
