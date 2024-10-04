cdf95b539d8e30ba41cc8e9b4831758d65f001c4
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-10-04 13:35:41 -0700

    one more fix...

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     end
   end
   resources :projects, except: [ :show ] do
-    get "/:project_id/deployments", to: "projects/deployments#index", as: :root
+    get "/deployments", to: "projects/deployments#index", as: :root
     resources :services, only: %i[index new create destroy update], module: :projects
     resources :metrics, only: [ :index ], module: :projects
     resources :project_add_ons, only: %i[create destroy], module: :projects
