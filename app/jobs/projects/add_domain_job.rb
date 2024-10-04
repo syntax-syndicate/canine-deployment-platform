c1ef039179d3da023e4334d4f5ebd95e470aea33
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-10-04 14:27:27 -0700

    small bug

--- app/jobs/projects/add_domain_job.rb
+++
@@ -1,7 +0,0 @@
-class Projects::AddDomainJob < ApplicationJob
-  def perform(domain)
-    cluster = domain.cluster
-    runner = Cli::RunAndLog.new(cluster)
-    K8::Kubectl.new(cluster.kubeconfig, runner).apply_yaml(K8::Stateless::Ingress.new(domain.service).to_yaml)
-  end
-end
--- app/jobs/services/add_domain_job.rb
+++ app/jobs/services/add_domain_job.rb
@@ -2,6 +2,6 @@ class Services::AddDomainJob < ApplicationJob
   def perform(domain)
     cluster = domain.cluster
     runner = Cli::RunAndLog.new(cluster)
-    K8::Kubectl.new(cluster.kubeconfig, runner).apply_yaml(K8::Stateless::Ingress.new(domain.project_service).to_yaml)
+    K8::Kubectl.new(cluster.kubeconfig, runner).apply_yaml(K8::Stateless::Ingress.new(domain.service).to_yaml)
   end
 end
