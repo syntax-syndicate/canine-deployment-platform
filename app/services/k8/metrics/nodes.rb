20aba179af0c8ca2eb34e6c60535ab65bd01ea66
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-10-07 13:52:18 -0700

    metrics

--- app/services/k8/metrics/nodes.rb
+++ app/services/k8/metrics/nodes.rb
@@ -1,7 +1,7 @@
 class K8::Metrics::Nodes
   def self.call(cluster)
     K8::Kubectl.new(cluster.kubeconfig, nil).with_kube_config do |kubeconfig_file|
-      command = "kubectl top nodes --kubeconfig #{kubeconfig_file.path}"
+      command = "kubectl get nodes -o json --kubeconfig #{kubeconfig_file.path}"
       output = `#{command}`
       parse_output(output)
     end
@@ -10,28 +10,15 @@ class K8::Metrics::Nodes
   private
 
   def self.parse_output(output)
-    # Define a regex pattern to capture the required fields
-    pattern = /(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/
-
-    # Split the data into lines and remove the header line
-    lines = output.split("\n")
-    header = lines.shift # Remove the header line
-
-    # Parse each line and create a hash for each node
-    parsed_data = lines.map do |line|
-      match = pattern.match(line)
-      if match
-        {
-          name: match[1],
-          cpu_cores: match[2],
-          cpu_percent: match[3],
-          memory_bytes: match[4],
-          memory_percent: match[5]
-        }
-      else
-        {}
-      end
+    data = JSON.parse(output)
+    data["items"].map do |node|
+      {
+        name: node.dig("metadata", "name"),
+        cpu_cores: node.dig("status", "capacity", "cpu"),
+        cpu_percent: node.dig("status", "allocatable", "cpu"),
+        memory_bytes: node.dig("status", "capacity", "memory"),
+        memory_percent: node.dig("status", "allocatable", "memory")
+      }
     end
-    parsed_data
   end
 end
