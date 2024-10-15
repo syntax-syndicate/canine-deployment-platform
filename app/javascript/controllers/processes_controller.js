c65c7557c400722e91cf0f8462ec2e02afa4c583
Chris Zhu <chris@chriss-mbp.lan>
2024-10-15 10:17:04 -0700

    processes

--- app/javascript/controllers/processes_controller.js
+++ app/javascript/controllers/processes_controller.js
@@ -1,13 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
+import tippy from "tippy.js";
+
 
 export default class extends Controller {
+  static targets = ["command"]
 
   connect() {
-    console.log("processes controller connected")
+  }
+
+  copyToClick(event) {
+    navigator.clipboard.writeText(this.commandTarget.textContent)
+    this.tooltip(event.target)
+  }
+
+  tooltip(target) {
+    tippy(target, {
+      content: "Copied!",
+      showOnCreate: true,
+      onHidden: (instance) => {
+        instance.destroy()
+      }
+    })
   }
 
   showConnectionInstructions(event) {
-    event.preventDefault()
+    event.preventDefault();
+    const text = `KUBECONFIG=/path/to/kubeconfig kubectl exec -it ${event.target.dataset.podName} -- /bin/bash`
+    this.commandTarget.textContent = text
     click_outside_modal.showModal()
   }
 }
