815929d7e32e355848a1d9656fa0cc753e865167
Chris Zhu <chris@chriss-mbp.lan>
2024-10-18 22:27:55 -0700

    utf 8 encoding

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Canine
     config.application_name = Rails.application.class.module_parent_name
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.encoding = "utf-8"
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
