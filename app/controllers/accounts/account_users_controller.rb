e1380b5436607d69248dd6c032b3c55aaf075beb
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-10-25 14:50:16 -0700

    add new accounts

--- app/controllers/accounts/account_users_controller.rb
+++ app/controllers/accounts/account_users_controller.rb
@@ -2,7 +2,7 @@ class Accounts::AccountUsersController < ApplicationController
   before_action :set_account
   def create
     user = User.find_or_initialize_by(email: user_params[:email]) do |user|
-      user.first_name = email.split("@").first
+      user.first_name = user_params[:email].split("@").first
       user.password = Devise.friendly_token[0, 20]
       user.save!
     end
