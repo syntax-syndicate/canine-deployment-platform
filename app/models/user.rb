9228fd444d75b69ee61d94b6b9f4786f54857546
Celina Lopez <marisa.celina.lopez4@gmail.com>
2024-10-29 10:16:40 -0700

    test todo

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,5 +40,4 @@ class User < ApplicationRecord
 
   # has_many :notifications, as: :recipient, dependent: :destroy, class_name: "Noticed::Notification"
   # has_many :notification_mentions, as: :record, dependent: :destroy, class_name: "Noticed::Event"
-  # TODO: new todo test
 end
