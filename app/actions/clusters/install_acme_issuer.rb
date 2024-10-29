97cc07ab9c91af9102519ee664ee6c345c606f95
Chris Zhu <chris@chriss-mbp.lan>
2024-10-28 17:06:07 -0700

    remove asdf

--- app/actions/clusters/install_acme_issuer.rb
+++ app/actions/clusters/install_acme_issuer.rb
@@ -9,12 +9,12 @@ class Clusters::InstallAcmeIssuer
     runner = Cli::RunAndLog.new(cluster)
     kubectl = K8::Kubectl.new(cluster.kubeconfig, runner)
     begin
-      kubectl.("get clusterissuer letsencryptasdf")
+      kubectl.("get clusterissuer letsencrypt")
       cluster.info("Acme issuer is already installed")
     rescue Cli::CommandFailedError => e
       cluster.info("Acme issuer not detected, installing...")
-      ingress_yaml = K8::Shared::AcmeIssuer.new(cluster.account.owner.email).to_yaml
-      kubectl.apply_yaml(ingress_yaml)
+      acme_issuer_yaml = K8::Shared::AcmeIssuer.new(cluster.account.owner.email).to_yaml
+      kubectl.apply_yaml(acme_issuer_yaml)
       cluster.info("Acme issuer installed")
     end
   rescue StandardError => e
