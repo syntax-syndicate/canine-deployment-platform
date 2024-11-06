8c916bccab053f625c7161891ed48f3f4b4a7b33
Chris Zhu <chris@chriss-mbp.lan>
2024-11-05 17:04:31 -0800

    fix ingress

--- app/services/k8/stateless/ingress.rb
+++ app/services/k8/stateless/ingress.rb
@@ -1,11 +1,14 @@
 class K8::Stateless::Ingress < K8::Base
   attr_accessor :service, :project, :domains, :cluster
-  delegate :name, to: :service
 
   def initialize(service)
     @service = service
     @project = service.project
     @cluster = @project.cluster
+  end
+
+  def name
+    "#{@service.name}-ingress"
   end
 
   def get_ingress
