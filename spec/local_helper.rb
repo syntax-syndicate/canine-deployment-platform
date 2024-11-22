0ce336f7c97c7fdef850d3331585ff8f7e3f0f37
Chris Zhu <chris@chriss-mbp.lan>
2024-11-22 11:21:13 -0800

    local system specs on docker

---
+++ spec/local_helper.rb
@@ -0,0 +1,12 @@
+# Configure RSpec and Capybara for local system tests
+RSpec.configure do |config|
+  config.before(:each, type: :system, file_path: %r{spec/system/local}) do
+    Capybara.app_host = 'http://localhost:3000'
+    Capybara.run_server = false
+    Capybara.default_host = 'localhost:3000'
+    
+    # Override default_url_options if they're set elsewhere
+    Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+    default_url_options[:host] = 'localhost:3000'
+  end
+end
---
+++ spec/system/local/initial_install_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+require 'local_helper'
+
+RSpec.describe "Initial Install", type: :system do
+  it "displays the initial install page" do
+    visit root_path
+    expect(page).to have_content("Please go to Github.com and enter your personal access token below.")
+  end
+end
