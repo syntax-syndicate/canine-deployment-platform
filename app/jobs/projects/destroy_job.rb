f614e1ddac99e7795a69464e06aa38f9cc8c84b7
Chris Zhu <chris@chriss-mbp.lan>
2024-11-30 14:40:59 -0800

    fix destroy project job

--- app/jobs/projects/destroy_job.rb
+++ app/jobs/projects/destroy_job.rb
@@ -5,16 +5,10 @@ class Projects::DestroyJob < ApplicationJob
     kubectl = K8::Kubectl.new(kubeconfig)
 
     kubectl.call("delete namespace #{project.name}")
+
     # Delete the github webhook for the project IF there are no more projects that refer to that repository
-
-
     unless Project.where(repository_url: project.repository_url).where.not(id: project.id).exists?
       remove_github_webhook(project)
-    end
-    project.events.destroy_all
-    project.builds.each do |build|
-      build.deployment.destroy!
-      build.destroy!
     end
     project.destroy!
   end
