4b7f9d6f6dc513be55a2d166fdfcbd60c6acce01
Chris Zhu <chris@chriss-mbp.lan>
2024-12-07 13:42:06 -0800

    make nginx max body size larger

--- resources/k8/scripts/install_nginx_ingress.sh
+++ resources/k8/scripts/install_nginx_ingress.sh
@@ -2,4 +2,4 @@ set -e
 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install ingress-nginx ingress-nginx/ingress-nginx --namespace $NAMESPACE
+helm install ingress-nginx ingress-nginx/ingress-nginx --namespace $NAMESPACE --set controller.config.proxy-body-size=0
