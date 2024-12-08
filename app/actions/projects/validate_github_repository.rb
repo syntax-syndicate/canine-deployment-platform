c51cda263187940fce275cf0bc56393cdd19c4b5
Chris Zhu <chris@chriss-mbp.lan>
2024-12-07 16:29:46 -0800

    show error message when github repository cant be accessed

--- app/actions/projects/validate_github_repository.rb
+++ app/actions/projects/validate_github_repository.rb
@@ -10,5 +10,8 @@ class Projects::ValidateGithubRepository
       context.project.errors.add(:repository_url, 'does not exist')
       context.fail_and_return!('Repository does not exist')
     end
+  rescue Octokit::Forbidden => e
+    context.project.errors.add(:repository_url, 'cannot be accessed')
+    context.fail_and_return!('Repository cannot be accessed, try logging out and in again')
   end
 end
