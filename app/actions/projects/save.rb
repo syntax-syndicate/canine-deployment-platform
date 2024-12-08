6d8a5bbddfbfcbda03dd40c57b757024cedf8759
Chris Zhu <chris@chriss-mbp.lan>
2024-12-08 00:12:17 -0800

    added better layouts

--- app/actions/projects/save.rb
+++ app/actions/projects/save.rb
@@ -6,6 +6,7 @@ class Projects::Save
 
   executed do |context|
     ActiveRecord::Base.transaction do
+      context.project.repository_url = context.project.repository_url.strip.downcase
       context.project.save!
       ProjectCredentialProvider.create!(project: context.project, provider: context.user.github_provider)
     end
