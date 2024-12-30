07e8df60a9d6e36dab3a86be88a4486dae605b89
Chris Zhu <chris@chriss-mbp.lan>
2024-12-30 11:33:58 -0800

    make user admin read only

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -6,7 +6,15 @@ ActiveAdmin.register User do
     column :num_projects do |user|
       user.projects.count
     end
+    actions defaults: true do |user|
+      link_to 'Login As', login_as_admin_user_path(user), method: :post
+    end
   end
 
   actions :all, except: []
+
+  member_action :login_as, method: :post do
+    sign_in(:user, User.find(params[:id]), bypass: true)
+    redirect_to root_path, notice: "Logged in as #{resource.email}"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ApplicationRecord
   has_many :projects, through: :accounts
   has_many :add_ons, through: :accounts
   has_many :services, through: :accounts
-
+  attr_readonly :admin
 
   # has_many :notifications, as: :recipient, dependent: :destroy, class_name: "Noticed::Notification"
   # has_many :notification_mentions, as: :record, dependent: :destroy, class_name: "Noticed::Event"
