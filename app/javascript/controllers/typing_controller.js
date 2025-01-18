18d21ca35908d00122d0b063e4ac6034f7f7c5ee
Chris Zhu <chris@chriss-mbp.lan>
2025-01-17 18:47:53 -0800

    update landing page

---
+++ app/javascript/controllers/typing_controller.js
@@ -0,0 +1,25 @@
+// Customizable command palette for advanced users
+// Opens with cmd+k or ctrl+k by default
+// https://github.com/excid3/ninja-keys
+
+import Typed from 'typed.js';
+
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["typing"]
+  connect() {
+    console.log("Hello");
+    const typed = new Typed(this.typingTarget, {
+      strings: [
+        '<b style="color: #7F4B8B">Sentry</b>',
+        '<b style="color: #4F42DB">Dagster</b>',
+        '<b style="color: #509EE2">Metabase</b>',
+        '<b style="color: #EF5A27">Grafana</b>',
+        '<b style="color: #E43820">Airflow</b>',
+      ],
+      typeSpeed: 50,
+      loop: true,
+    });
+  }
+}
