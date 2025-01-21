a475aa4b43dcacad958c66321a8e55fa2bae489d
Chris Zhu <chris@chriss-mbp.lan>
2025-01-20 18:39:15 -0800

    increase number of puma workers

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,6 @@ plugin :tmp_restart
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# Set the number of worker processes.
+workers ENV.fetch("WEB_CONCURRENCY", 2)
