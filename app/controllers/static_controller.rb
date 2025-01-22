19257307429968d413a897a6f58abfa01c8d7b40
Chris Zhu <chris@chriss-mbp.lan>
2025-01-22 09:31:05 -0800

    added documentation

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,7 @@
 class StaticController < ApplicationController
-  MANIFESTO_FILE = Rails.root.join("public", "manifesto.md")
   INSTALL_SCRIPT = "curl -sSL https://raw.githubusercontent.com/czhu12/canine/refs/heads/main/install/install.sh | bash"
   skip_before_action :authenticate_user!
 
   def index
-  end
-
-  def manifesto
-    @content = File.read(MANIFESTO_FILE)
-    renderer = Redcarpet::Render::HTML.new
-    @markdown = Redcarpet::Markdown.new(renderer, tables: true)
   end
 end
