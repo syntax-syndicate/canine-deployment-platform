f26a7af5c8df437855f59d4cd86d44797750b956
Celina Lopez <marisa.celina.lopez4@gmail.com>
2025-01-24 10:41:23 -0800

    revert async metrics

--- app/controllers/clusters/metrics_controller.rb
+++ app/controllers/clusters/metrics_controller.rb
@@ -4,6 +4,7 @@ class Clusters::MetricsController < Clusters::BaseController
   before_action :set_cluster
 
   def show
+    @nodes = K8::Metrics::Api::Node.ls(@cluster)
     @metrics = @cluster.metrics.node_only_tags.order(created_at: :desc).limit(1000)
   end
 
