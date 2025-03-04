91fcf965337620d2904d4673841d81ba20fcb9de
Chris Zhu <chris@chriss-mbp.lan>
2025-03-04 02:29:04 -0800

    quote multi word env vars

--- app/jobs/projects/build_job.rb
+++ app/jobs/projects/build_job.rb
@@ -59,7 +59,7 @@ class Projects::BuildJob < ApplicationJob
 
     # Add environment variables to the build command
     project.environment_variables.each do |envar|
-      docker_build_command.push("--build-arg", "#{envar.name}=#{Shellwords.escape(envar.value)}")
+      docker_build_command.push("--build-arg", "#{envar.name}=\"#{envar.value}\"")
     end
 
     # Add the build context directory at the end
