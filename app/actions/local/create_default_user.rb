c440bad6e1f6d9f3b73c6c4c14737d647ba646f3
Chris Zhu <chris@chriss-mbp.lan>
2025-03-19 09:17:07 -0700

    fix providers in local mode

--- app/actions/local/create_default_user.rb
+++ app/actions/local/create_default_user.rb
@@ -6,10 +6,12 @@ class Local::CreateDefaultUser
 
   executed do |context|
     ActiveRecord::Base.transaction do
-      context.user = User.first || User.new
-      context.user.email = "#{ENV["CANINE_USERNAME"].presence || SecureRandom.uuid}@#{BASE_DOMAIN}"
-      context.user.password = ENV["CANINE_PASSWORD"].presence || "password"
-      context.user.password_confirmation = ENV["CANINE_PASSWORD"].presence || "password"
+      context.user = User.first || User.new(
+        email: "#{ENV["CANINE_USERNAME"].presence || SecureRandom.uuid}@#{BASE_DOMAIN}",
+        password: ENV["CANINE_PASSWORD"].presence || "password",
+        password_confirmation: ENV["CANINE_PASSWORD"].presence || "password",
+        admin: true
+      )
       context.user.save!
       context.account = context.user.accounts.first || Account.create!(name: "Default", owner: context.user)
       context.account.account_users.find_or_create_by!(user: context.user)
