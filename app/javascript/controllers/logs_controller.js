f382a3ab2b55b031c8b6191a04507f1c050dfbb1
Chris Zhu <chriszhu@Chriss-MacBook-Pro-2.local>
2025-04-06 21:08:31 -0700

    added log streaming

--- app/javascript/controllers/logs_controller.js
+++ app/javascript/controllers/logs_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
+import { get } from "@rails/request.js"
 
+const REFRESH_INTERVAL = 4000;
 export default class extends Controller {
   static targets = ["container"]
 
@@ -9,6 +11,24 @@ export default class extends Controller {
 
     // Add event listener for Turbo Frame load
     document.addEventListener('turbo:frame-load', this.scrollToBottom.bind(this));
+    this.interval = setInterval(() => {
+      // Fetch new logs
+      this.loadNewLogs();
+    }, REFRESH_INTERVAL);
+  }
+
+  disconnect() {
+    clearInterval(this.interval);
+  }
+
+  async loadNewLogs() {
+    // Only do this if the scroll is at the bottom
+    if (this.containerTarget.scrollTop === this.containerTarget.scrollHeight) {
+      await get(this.element.dataset.url, {
+        responseKind: 'turbo-stream'
+      });
+      this.scrollToBottom();
+    }
   }
 
   scrollToBottom() {
