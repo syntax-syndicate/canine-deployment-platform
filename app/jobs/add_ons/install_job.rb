94225f7bbc98be898dd8885b6fb6c9aa5be471e2
Chris <chris@example.com>
2025-05-16 20:30:44 -0700

    refactor services, restart on install & update

--- app/jobs/add_ons/install_job.rb
+++ app/jobs/add_ons/install_job.rb
@@ -2,5 +2,7 @@ class AddOns::InstallJob < ApplicationJob
   def perform(add_on)
     AddOns::InstallHelmChart.execute(add_on:)
     # TODO: Restart the service
+    service = K8::Helm::Service.create_from_add_on(add_on)
+    service.restart
   end
 end
