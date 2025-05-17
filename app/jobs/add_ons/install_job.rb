9935181424a94055274e78c04c5cfa041b45cc22
Chris <chris@example.com>
2025-05-16 23:26:24 -0700

    restart only on updates

--- app/jobs/add_ons/install_job.rb
+++ app/jobs/add_ons/install_job.rb
@@ -1,8 +1,11 @@
 class AddOns::InstallJob < ApplicationJob
   def perform(add_on)
+    needs_restart = add_on.installed?
     AddOns::InstallHelmChart.execute(add_on:)
-    # TODO: Restart the service
-    service = K8::Helm::Service.create_from_add_on(add_on)
-    service.restart
+
+    if needs_restart
+      service = K8::Helm::Service.create_from_add_on(add_on)
+      service.restart
+    end
   end
 end
